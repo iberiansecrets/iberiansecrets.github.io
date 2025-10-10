ad00e378ad951b6e6205b45204901c859ca83b68
carlosmartinhernandez79 <carlosmartinhernandez79@gmail.com>
2025-10-10 14:53:57 +0200

    Versión última con Linktree y Contacto

--- js/local_js.js
+++ js/local_js.js
@@ -22,3 +22,35 @@ function showContact(){
     document.getElementById("contacto").classList.remove("d-none");
 }
 
+function LinkTree(){
+    const url = "https://linktr.ee/iberiansecrets"; // Reemplaza con tu URL deseada
+    window.open(url, "_blank");
+}
+
+function Instagram(){
+    const url = "https://instagram.com/Iberiansecrets"; // Reemplaza con tu URL deseada
+    window.open(url, "_blank");
+}
+
+function Youtube(){
+    const url = "https://www.youtube.com/@IberianSecrets"; // Reemplaza con tu URL deseada
+    window.open(url, "_blank");
+}
+
+function XTwitter(){
+    const url = "https://x.com/Iberiansecrets"; // Reemplaza con tu URL deseada
+    window.open(url, "_blank");
+}
+
+function TikTok(){
+    const url = "https://tiktok.com/@iberiansecrets"; // Reemplaza con tu URL deseada
+    window.open(url, "_blank");
+}
+
+function sendEmail() {
+    const destinatario = "iberiansecrets@gmail.com";
+    const asunto = "Consulta";
+    
+    const mailtoLink = 'mailto:${destinatario}?subject=${encodeURIComponent(asunto)}';
+    window.location.href = mailtoLink;
+    }
